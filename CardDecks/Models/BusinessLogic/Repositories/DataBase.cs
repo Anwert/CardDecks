d693da108582fae39d9d1f5fd18b8cae26f27a22
f.krutakov <f.krutakov@eastwind.ru>
2020-07-29 01:23:15 +0500

    убрал бесполезный лок

--- CardDecks/Models/BusinessLogic/Repositories/DataBase.cs
+++ CardDecks/Models/BusinessLogic/Repositories/DataBase.cs
@@ -8,19 +8,6 @@ namespace CardDecks.Models.BusinessLogic.Repositories
 	/// </summary>
 	public static class DataBase
 	{
-		private static readonly object LockObj = new object();
-
-		private static List<CardDeck> _cardDecks = new List<CardDeck>();
-
-		public static List<CardDeck> CardDecks
-		{
-			get
-			{
-				lock (LockObj)
-				{
-					return _cardDecks;
-				}
-			}
-		}
+		public static List<CardDeck> CardDecks { get; } = new List<CardDeck>();
 	}
 }
